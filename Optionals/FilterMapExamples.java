7b2ba68b0924da9846344f212857d4bd3856a73e
Atul Singh <atul@Atuls-MacBook-Pro.local>
2022-06-10 18:07:22 +0530

    ParallelStream and Optionals

---
+++ Optionals/FilterMapExamples.java
@@ -0,0 +1,43 @@
+package Optionals;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.Optional;
+
+public class FilterMapExamples {
+
+    public static void optionalFilter() {
+        Optional<Student> studentOptional = Optional.ofNullable(StudentDataBase.studentSupplier.get());
+        studentOptional.filter(student -> student.getGpa()>=3.5)
+                .ifPresent(student -> System.out.println(student));
+    }
+
+
+    public static void optionalMap() {
+        Optional<Student> studentOptional = Optional.ofNullable(StudentDataBase.studentSupplier.get());
+
+        if(studentOptional.isPresent()) {
+            Optional<String> stringOptional = studentOptional.filter(student -> student.getGpa()>=3.5)
+                    .map(Student::getName);
+
+            System.out.println(stringOptional.get());
+        }
+    }
+
+    public static void optionalFlatMap() {
+        Optional<Student> studentOptional = Optional.ofNullable(StudentDataBase.studentSupplier.get());
+
+        Optional<String> name = studentOptional.filter(student -> student.getGpa()>=3.5)
+                .flatMap(Student::getBike)
+                .map(Bike::getName);
+
+        name.ifPresent(s -> System.out.println("Name: "+s));
+    }
+
+    public static void main(String[] args) {
+        optionalFilter();
+        optionalMap();
+        optionalFlatMap();
+    }
+}
